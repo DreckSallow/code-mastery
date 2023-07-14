4351b886c1a621fa5c1cfc1ac491f517fdf633aa
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-07-13 21:59:19 -0500

    feat: add insertion algorithm

---
+++ algorithms/sorting/src/insertion/mod.rs
@@ -0,0 +1,38 @@
+pub fn sort_with_insertion<T>(list: &mut Vec<T>)
+where
+    T: PartialOrd + Copy,
+{
+    if list.len() < 2 {
+        return;
+    }
+    for i in 1..list.len() {
+        let current_val = list[i];
+
+        let mut j = (i as i32) - 1;
+
+        while j >= 0 && (list[j as usize] > current_val) {
+            list[(j + 1) as usize] = list[j as usize];
+            j -= 1;
+        }
+        list[(j + 1) as usize] = current_val;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::sort_with_insertion;
+
+    #[test]
+    fn allow_less_length() {
+        let mut less_list = vec![9];
+        sort_with_insertion(&mut less_list);
+        assert_eq!(less_list, vec![9]);
+    }
+
+    #[test]
+    fn test_numbers() {
+        let mut list_to_sort = vec![8, 4, 5, 1, 6, 0, 7];
+        sort_with_insertion(&mut list_to_sort);
+        assert_eq!(list_to_sort, vec![0, 1, 4, 5, 6, 7, 8]);
+    }
+}
