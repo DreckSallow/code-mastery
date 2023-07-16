eb2f47b5b806fac3bad91b9dc43e4e1f9642ce51
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-07-16 17:17:00 -0500

    feat: add insertion sort worst case

--- algorithms/sorting/src/insertion/mod.rs
+++ algorithms/sorting/src/insertion/mod.rs
@@ -35,4 +35,11 @@ mod test {
         sort_with_insertion(&mut list_to_sort);
         assert_eq!(list_to_sort, vec![0, 1, 4, 5, 6, 7, 8]);
     }
+
+    #[test]
+    fn worst_case() {
+        let mut list_to_sort = vec![9, 8, 7, 6, 5, 4, 3, 2, 1];
+        sort_with_insertion(&mut list_to_sort);
+        assert_eq!(list_to_sort, vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
 }
